6604b6d30819d6da85ad9ff13767c0ba0b056b94
zpt <zpt@zpt.com.br>
2023-09-20 22:49:46 -0300

    Questão 10 finalizada

--- src/Department.php
+++ src/Department.php
@@ -3,11 +3,13 @@ namespace Department;
 
 require_once './User.php';
 
+use User;
+
 class Department {
 	private User\User $user;
 
 	public function __construct() {
-		$this->user = new User\User(); // @todo fixme
+		$this->user = new User\User(); // Foi corrigido após inserir o namespace User no namespace Department. Estava sem o namespace User incluído.
 	}
 
 	public function setDb($db) {
